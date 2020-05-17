be148ee8470da18a2fba9292b77be2daa3f10af3
tashfeenrao <raotashfeen25@gmail.com>
2020-05-17 21:53:16 +0500

    instal axios package and first api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import '../css/style.css';
+import axios from 'axios';
+
+async function searchRecepie(query) {
+  try {
+    const rec = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+    const data = rec.data.recipes;
+  } catch (error) {
+    alert(error);
+  }
+}
+searchRecepie('grape');
