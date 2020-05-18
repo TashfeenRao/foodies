3a41509be6cb06050acb68f58f4bed5f15c11d59
tashfeenrao <raotashfeen25@gmail.com>
2020-05-19 01:30:33 +0500

    setup search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
 import '../css/style.css';
 import Search from './models/Search';
 
+const state = {};
+const searchController = async () => {
+  const query = 'pizza';
+  if (query) {
+    state.search = new Search(query);
+    await state.search.getRecipes();
+  }
+};
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  searchController();
+});
+
 const search = new Search('pizza');
 search.getRecipes();
